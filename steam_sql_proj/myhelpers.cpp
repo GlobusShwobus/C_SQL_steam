09830c7eec53b1eca3e0aa98bef55863a73e084e
GlobusShwobus <fjodorzaro@gmail.com>
2025-07-02 22:54:56 +0300

    fastest recov in the west (wtf was that crash)

---
+++ steam_sql_proj/myhelpers.cpp
@@ -0,0 +1,80 @@
+#include "myhelpers.h"
+#include <windows.h>
+#include <iostream>
+#include <iomanip>
+#include <chrono>
+#include <format>
+namespace ORDO {
+	namespace lazy {
+        void RemoveSymbols(std::string& from, const std::string& symbols) {
+            from.erase(std::remove_if(from.begin(), from.end(), [&symbols](const char c) {
+                return symbols.find(c) != std::string::npos;
+                }), from.end());
+        }
+        void RemoveSymbols(std::string& from, const char symbol) {
+            from.erase(std::remove_if(from.begin(), from.end(), [symbol](const char c) {
+                return symbol == c;
+                }), from.end());
+        }
+        void PrintList(const std::vector<std::pair<std::string, std::string>>& list) {
+            size_t i = 1;
+            for (const auto& each : list) {
+                std::cout << std::setw(5) << i++ << " " << each.second << "\n";
+            }
+        }
+        bool ListContains(const std::vector<std::string>& list, const std::string& lookFor) {
+            return std::find(list.begin(), list.end(), lookFor) != list.end();
+        }
+        const std::string UnixTime(time_t unix_timestamp) {
+            std::chrono::system_clock::time_point tp = std::chrono::system_clock::from_time_t(unix_timestamp);
+            return std::format("{:%Y-%m-%d %H:%M:%S}", std::chrono::floor<std::chrono::seconds>(tp));
+        }
+        std::string GetClipboard() {
+            if (!OpenClipboard(nullptr)) {
+                return "";
+            }
+            HANDLE hData = GetClipboardData(CF_TEXT);
+            if (hData == nullptr) {
+                CloseClipboard();
+                return "";
+            }
+
+            char* copy_stream = static_cast<char*>(GlobalLock(hData));//do not delete OR MEMES
+            std::string text;
+
+            if (copy_stream) {
+                text = copy_stream;
+            }
+
+            GlobalUnlock(hData);
+            CloseClipboard();
+
+            return text;
+        }
+
+        void console_wait() {
+            std::cout << "Press Enter to continue...";
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            std::cin.get();
+        }
+        void console_title() {
+            printf("\n%s", R"( @@@@@@   @@@@@@@   @@@@@@@    @@@@@@   
+@@@@@@@@  @@@@@@@@  @@@@@@@@  @@@@@@@@  
+@@!  @@@  @@!  @@@  @@!  @@@  @@!  @@@  
+!@!  @!@  !@!  @!@  !@!  @!@  !@!  @!@  
+@!@  !@!  @!@!!@!   @!@  !@!  @!@  !@!  
+!@!  !!!  !!@!@!    !@!  !!!  !@!  !!!  
+!!:  !!!  !!: :!!   !!:  !!!  !!:  !!!  
+:!:  !:!  :!:  !:!  :!:  !:!  :!:  !:!  
+::::: ::  ::   :::   :::: ::  ::::: ::  
+ : :  :    :   : :  :: :  :    : :  :)");
+
+            printf("%s", "\n\n=====================================================================================");
+        }
+        void console_clear() {
+            system("cls");
+        }
+
+
+	}
+}
--- steam_sql_proj/myhelpers.h
+++ steam_sql_proj/myhelpers.h
@@ -1,1 +1,22 @@
 #pragma once
+#include <string>
+#include <vector>
+
+#define NOMINMAX
+
+namespace ORDO {
+
+	namespace lazy {
+        void RemoveSymbols(std::string& from, const std::string& symbols);
+        void RemoveSymbols(std::string& from, const char symbol);
+        void PrintList(const std::vector<std::pair<std::string, std::string>>& list);
+        bool ListContains(const std::vector<std::string>& list, const std::string& lookFor);
+        const std::string UnixTime(time_t unix_timestamp);
+        std::string GetClipboard();
+
+        void console_wait();
+        void console_title();
+        void console_clear();
+	}
+
+}
